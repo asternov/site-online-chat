cc1b46f03eb4f11278fc445645d8f7806f82c852
DoroshenkoDevelop <pasha@tass.by>
2022-06-09 18:14:52 +0600

    chat validate

--- resources/js/components/ChatForm.vue
+++ resources/js/components/ChatForm.vue
@@ -10,10 +10,11 @@
             v-model="name"
         />
             <span class="input-group-btn d-inline-block">
-      <button class="btn btn-primary btn-sm" id="btn-chat" @click="sendMessage">
+      <button class="btn btn-primary btn-sm" id="btn-chat" @click="sendMessage" :disabled="!canSend">
         Send
-      </button>
+      </button><span v-if="showAlert" class="text-danger">wait... </span>
     </span>
+
         <input
             id="btn-input"
             type="text"
@@ -33,15 +34,52 @@ export default {
         return {
             newMessage: "",
             name: "",
+            timestamp: 0,
+            msgCount: 0,
+            showAlert: false,
         };
+    },
+    computed: {
+        canSend() {
+            if (!this.name.length()) {
+                return false;
+            } else if (!this.newMessage.length()) {
+                return false;
+            } else if (this.showAlert) {
+                return false;
+            }
+
+            return true;
+        }
     },
     methods: {
         sendMessage() {
+            var newTimestamp = Date.now() / 10000 | 0;
+
+            if (newTimestamp == this.timestamp) {
+                if (this.msgCount >= 2) {
+                    this.timestamp = newTimestamp + 1
+                    this.showAlert = true;
+                    var pause = (newTimestamp + 2) * 10000 - Date.now()
+                    setTimeout(() => {
+                        this.showAlert = false;
+                        this.msgCount = 0;
+                    }, pause)
+
+                    return null;
+                }
+
+                this.msgCount++;
+            }
+
+            this.timestamp = newTimestamp;
+
             this.$emit("messagesent", {
                 sender: this.name,
                 author: {name: this.name},
                 message: this.newMessage,
             });
+
             this.newMessage = "";
         }
     },
