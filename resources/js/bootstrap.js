a260fbacd8a5297ade26f1fb63db3f3819cc6719
DoroshenkoDevelop <pasha@tass.by>
2022-06-21 17:10:07 +0600

    auto receive

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -21,7 +21,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 import Echo from 'laravel-echo';
-
 window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
